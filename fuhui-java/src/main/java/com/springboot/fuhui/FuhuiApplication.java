8af05f72642acbef7cb7a7c3b035c675eee88e19
meteor1993 <inwsy@hotmail.com>
2018-11-03 17:14:08 +0800

    删除部分无效import

--- fuhui-java/src/main/java/com/springboot/fuhui/FuhuiApplication.java
+++ fuhui-java/src/main/java/com/springboot/fuhui/FuhuiApplication.java
@@ -1,11 +1,7 @@
 package com.springboot.fuhui;
 
-import me.chanjar.weixin.mp.api.WxMpService;
-import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FuhuiApplication {
